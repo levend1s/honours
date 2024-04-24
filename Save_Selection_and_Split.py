48c6279749a827a12cfb2e0bb0cb9ec2b82457dc
levend1s <joshualevendis@Joshuas-MacBook-Air.local>
2024-04-24 16:25:15 +1000

    works with different channel numbers, works with z-stacks

--- Save_Selection_and_Split.py
+++ Save_Selection_and_Split.py
@@ -3,26 +3,43 @@ from ij.plugin.frame import RoiManager
 from ij import CompositeImage
 from ij.gui import ShapeRoi
 import os
+from itertools import product
 
 imp = IJ.getImage()
-image_dir = IJ.getDirectory("Image")
+image_dir = IJ.getDirectory("image")
 out_dir = image_dir + imp.getTitle() + "_split/"
 
 if not os.path.exists(out_dir):
 	os.makedirs(out_dir)
 
-channel_combinations = ["100", "010", "001", "011", "110", "101", "111"]
+# magic
+def getChannelCombinations(n):
+	r = []
+	tups = [i for i in product(range(2), repeat=n)]
+	for t in tups:
+		r.append("".join(str(x) for x in t))
+	
+	r.pop(0) # remove the one with all 0s
+	return r
 
-rm = RoiManager.getRoiManager()
-temp_roi = imp.getRoi()
-imp.setRoi(temp_roi)
-rm.addRoi(temp_roi)
+num_channels = imp.getNChannels()
 
-for c in channel_combinations:
-	imp_1 = imp.duplicate()
-	imp_1.setActiveChannels(c)
-	imp_1.cropAndSave([temp_roi], out_dir + imp.getTitle() + "_" + c + "_", "save png")
-
-rm.select(rm.getRoiIndex(temp_roi))
-rm.runCommand("delete")
-rm.close()
+if num_channels > 4:
+	print("too many channels! untested behaviour, exiting...")
+	exit(1)
+else:
+	channel_combinations = getChannelCombinations(num_channels)
+	rm = RoiManager.getRoiManager()
+	temp_roi = imp.getRoi()
+	imp.setRoi(temp_roi)
+	rm.addRoi(temp_roi)
+	
+	for c in channel_combinations:
+		imp_1 = imp.duplicate()
+		imp_1.setZ(imp.getZ())
+		imp_1.setActiveChannels(c)
+		imp_1.cropAndSave([temp_roi], out_dir + imp.getTitle() + "_" + c + "_", "save png")
+	
+	rm.select(rm.getRoiIndex(temp_roi))
+	rm.runCommand("delete")
+	rm.close()
