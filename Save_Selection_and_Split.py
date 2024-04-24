fd705dd1fdcc23d840b7256ddfb9f9ce85b7fe7f
levend1s <joshualevendis@Joshuas-MacBook-Air.local>
2024-04-24 14:28:44 +1000

    save to dir

--- Save_Selection_and_Split.py
+++ Save_Selection_and_Split.py
@@ -2,14 +2,18 @@ from ij import IJ
 from ij.plugin.frame import RoiManager
 from ij import CompositeImage
 from ij.gui import ShapeRoi
+import os
 
 imp = IJ.getImage()
-dir = IJ.getDirectory("Image")
+image_dir = IJ.getDirectory("Image")
+out_dir = image_dir + imp.getTitle() + "_split/"
+
+if not os.path.exists(out_dir):
+	os.makedirs(out_dir)
+
+channel_combinations = ["100", "010", "001", "011", "110", "101", "111"]
 
 rm = RoiManager.getRoiManager()
-
-channel_combinations = ["100", "010", "001", "011", "111"]
-
 temp_roi = imp.getRoi()
 imp.setRoi(temp_roi)
 rm.addRoi(temp_roi)
@@ -17,7 +21,7 @@ rm.addRoi(temp_roi)
 for c in channel_combinations:
 	imp_1 = imp.duplicate()
 	imp_1.setActiveChannels(c)
-	imp_1.cropAndSave([temp_roi], dir + imp.getTitle() + "_" + c + "_", "save png")
+	imp_1.cropAndSave([temp_roi], out_dir + imp.getTitle() + "_" + c + "_", "save png")
 
 rm.select(rm.getRoiIndex(temp_roi))
 rm.runCommand("delete")
